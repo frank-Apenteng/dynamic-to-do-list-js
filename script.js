5efd07c995938f02586a3d8547758727bd232841
Frank Apenteng <frankapenteng7@gmail.com>
2025-01-26 20:19:54 +0000

    added classlist.add at line 23

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Create a remove button for the task
     const removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
-    removeButton.className = "remove-btn";
+    removeButton.classList.add("remove-btn");
 
     // Assign an onclick event to the remove button
     removeButton.onclick = function () {
